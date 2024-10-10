60b634c442e7bcc67b2d5aa380d4c1d4b8ab389e
MS <madhsa1973@gmail.com>
2024-10-10 17:25:46 -0400

    Login Page Works-Need to modify header

--- requestify/src/App.tsx
+++ requestify/src/App.tsx
@@ -4,12 +4,14 @@ import SignUp from './components/SignUp';
 import Login from './components/Login';
 
 const App: React.FC = () => {
-    const [isSignUp, setIsSignUp] = useState<boolean>(true);
+    const [isSignUp, setIsSignUp] = useState<boolean>(false); // Set default to false
 
     return (
         <div className="App">
-            <button onClick={() => setIsSignUp(true)}>Sign Up</button>
-            <button onClick={() => setIsSignUp(false)}>Login</button>
+            <div className="toggle-buttons">
+                <button onClick={() => setIsSignUp(false)}>Login</button>
+                <button onClick={() => setIsSignUp(true)}>Sign Up</button>
+            </div>
             {isSignUp ? <SignUp /> : <Login />}
         </div>
     );
