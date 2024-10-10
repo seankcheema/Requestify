33e59641c4a849d6d6906c65402709ef13f805fa
Madhav Sagi <madhsa1973@gmail.com>
2024-10-09 21:35:02 -0400

    Created a Login/Register Components

--- requestify/src/App.tsx
+++ requestify/src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    const [isSignUp, setIsSignUp] = useState<boolean>(true);
+
+    return (
+        <div className="App">
+            <button onClick={() => setIsSignUp(true)}>Sign Up</button>
+            <button onClick={() => setIsSignUp(false)}>Login</button>
+            {isSignUp ? <SignUp /> : <Login />}
+        </div>
+    );
+};
 
 export default App;
---
+++ requestify/src/components/Login.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login: React.FC = () => {
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [message, setMessage] = useState<string>('');
+
+    const handleLogin = async (event: React.FormEvent) => {
+        event.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:5000/login', {
+                username,
+                password
+            });
+            setMessage(response.data.message);
+        } catch (error) {
+            if (axios.isAxiosError(error) && error.response) {
+                setMessage(error.response.data.message);
+            } else {
+                setMessage('An error occurred');
+            }
+        }
+    };
+
+    return (
+        <div className="login">
+            <h2>Login</h2>
+            <form onSubmit={handleLogin}>
+                <div className="form-group">
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+            {message && <p>{message}</p>}
+        </div>
+    );
+};
+
+export default Login;
