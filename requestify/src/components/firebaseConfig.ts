1cec56ee6d0481644b7578fca8a17b6452319fb1
JoeParimucha <73606621+JoeParimucha@users.noreply.github.com>
2024-10-26 14:45:08 -0400

    Removed API Key Console Log
    Printed API Key to Console Log

--- requestify/src/components/firebaseConfig.ts
+++ requestify/src/components/firebaseConfig.ts
@@ -11,6 +11,6 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
-console.log('API Key:', process.env.REACT_APP_FIREBASE_API_KEY);
+//console.log('API Key:', process.env.REACT_APP_FIREBASE_API_KEY);
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
