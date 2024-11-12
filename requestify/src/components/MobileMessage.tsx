afa6c66e7e96bf940cb963429f707370ea357b0e
seancheema <92892168+seankcheema@users.noreply.github.com>
2024-11-12 15:59:39 -0500

    Message formatting mobile

--- requestify/src/components/MobileMessage.tsx
+++ requestify/src/components/MobileMessage.tsx
@@ -2,13 +2,46 @@ import React, { useEffect, useState } from 'react';
 import { FaHome, FaChartLine, FaDollarSign, FaBell } from 'react-icons/fa';
 import { useNavigate, useParams } from 'react-router-dom';
 import { io, Socket } from 'socket.io-client';
+import { useDJ } from './DJContext';
+import './MobileMessage.css';
 
 const ipAddress = process.env.REACT_APP_API_IP; // Use the environment variable
 
 const MobileMessage: React.FC = () => {
   const navigate = useNavigate();
-  const { djName: paramDJName } = useParams<{ djName: string }>();
   const [messages, setMessages] = useState<{ text: string; sent: boolean }[]>([]);
+  const [displayName, setDisplayName] = useState(''); // State for display name
+  const { djName: paramDJName } = useParams<{ djName: string }>(); // Get djName from URL params
+  const { djName, setDJName } = useDJ();
+
+  // Set djName in context whenever paramDJName changes
+  useEffect(() => {
+      if (paramDJName) {
+          setDJName(paramDJName);
+      }
+  }, [paramDJName, setDJName]);
+  
+
+  // Fetch the display name whenever djName changes
+  useEffect(() => {
+      const fetchDisplayName = async () => {
+          try {
+              const response = await fetch(`http://localhost:5001/dj/displayName/${djName}`);
+              if (response.ok) {
+                  const data = await response.json();
+                  setDisplayName(data.displayName);
+              } else {
+                  console.error('Failed to fetch display name');
+              }
+          } catch (error) {
+              console.error('Error fetching display name:', error);
+          }
+      };
+
+      if (djName) {
+          fetchDisplayName();
+      }
+  }, [djName]);
 
   useEffect(() => {
     // Connect to the socket server using dynamic IP
@@ -31,31 +64,42 @@ const MobileMessage: React.FC = () => {
   const goToPayment = () => navigate(`/dj/${paramDJName}/payment`);
 
   return (
-    <div className="message-container">
+    <div className="mobile-container">
       <header className="mobile-header">
         <div className="header-title">
-          <img src="/assets/requestify-logo.svg" alt="Requestify Logo" className="mobile-header-logo" onClick={goToHome} />
+          <img
+            src="/assets/requestify-logo.svg"
+            alt="Requestify Logo"
+            className="mobile-header-logo"
+            onClick={goToHome}
+          />
         </div>
       </header>
 
-      <main className="message-content">
-        <div className="message-box">
-          {messages.map((msg, index) => (
-            <div className="message-bubble" key={index}>
-              <p className="message-sender">DJ Grant</p>
-              <div className="message-text">{msg.text}</div>
+      <main className="mobile-content">
+        <section className="mobile-queue">
+          <h2>Messsages from {displayName || djName}</h2>
+          <div className="mobile-song-container">
+            <div className="mobile-song-list">
+            {messages.map((msg, index) => (
+            <div className="mobile-message-bubble" key={index}>
+              <div className="mobile-message-text">{msg.text}</div>
             </div>
           ))}
-        </div>
+            </div>
+          </div>
+        </section>
       </main>
 
-      <footer className="message-footer">
+
+
+      <footer className="mobile-footer">
         <nav className="bottom-nav">
           <div className="nav-item" onClick={goToHome}>
             <FaHome />
             <span>Home</span>
           </div>
-          <div className="nav-item" onClick={goToActivity}>
+          <div className="nav-item">
             <FaChartLine />
             <span>Activity</span>
           </div>
--- requestify/src/components/Queue.tsx
+++ requestify/src/components/Queue.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Dashboard.css';
 
-// Define a type for the props
 interface QueueProps {
   djName: string;
 }
 
-// Define a type for the track data, for reference only
 interface Track {
   trackName: string;
   artist: string;
@@ -24,11 +21,9 @@ const Queue: React.FC<QueueProps> = ({ djName }) => {
   const fetchTracks = async () => {
     try {
       const response = await fetch(`http://${ipAddress}:5001/tracks/${djName}`);
-      
       if (!response.ok) {
         throw new Error(`Error: ${response.status}`);
       }
-
       const data: string[][] = await response.json();
       setTracks(data);
     } catch (error) {
@@ -44,19 +39,21 @@ const Queue: React.FC<QueueProps> = ({ djName }) => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ djName, trackName, artist })
-    });
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status}`);
-    }
-
-    setTracks(tracks.filter(track => track[0] !== trackName && track[1] !== artist));
-    } 
-    catch (error) {
+      });
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      setTracks(tracks.filter(track => track[0] !== trackName && track[1] !== artist));
+    } catch (error) {
       console.error("Failed to remove track:", error);
     }
   };
 
   const removeAllTracks = async () => {
+    // Show a confirmation popup
+    const confirmed = window.confirm("Are you sure you want to clear the queue?");
+    if (!confirmed) return; // Exit if user cancels
+
     try {
       const response = await fetch(`http://${ipAddress}:5001/tracks/delete-all`, {
         method: 'DELETE',
@@ -64,28 +61,21 @@ const Queue: React.FC<QueueProps> = ({ djName }) => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ djName })
-    });
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status}`);
-    }
-
-    setTracks([]);
-    } 
-    catch (error) {
+      });
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      setTracks([]);
+    } catch (error) {
       console.error("Failed to remove all tracks:", error);
     }
   };
 
   useEffect(() => {
-    // Initial fetch
     fetchTracks();
-
-    // Set up polling interval
     const intervalId = setInterval(() => {
       fetchTracks();
-    }, 5000); // Fetch every 5 seconds (adjust as needed)
-
-    // Clear the interval on component unmount
+    }, 5000);
     return () => clearInterval(intervalId);
   }, [djName]);
 
@@ -99,129 +89,23 @@ const Queue: React.FC<QueueProps> = ({ djName }) => {
               <div key={index} className="song-item">
                 <img src={track[4]} alt={`${track[2]} cover`} className="album-cover" />
                 <div className="song-info">
-                  <p>{track[0]}</p> {/* Track name */}
-                  <p className="artist">{track[1]}</p> {/* Artist name */}
+                  <p>{track[0]}</p>
+                  <p className="artist">{track[1]}</p>
                 </div>
                 <div className="song-upvotes">
                   {track[5]}
                 </div>
                 <img src="/assets/Remove icon.svg" alt="Remove button" className="remove-btn" onClick={() => removeTrack(track[0], track[1])}/>
               </div>
-              
             ))
           ) : (
             <p>No tracks in the queue.</p>
           )}
         </div>
       </div>
-      <button className="clear-queue" onClick={() => removeAllTracks()}>Clear Queue</button>
+      <button className="clear-queue" onClick={removeAllTracks}>Clear Queue</button>
     </section>
   );
 };
-
-
-// const Queue: React.FC = () => {
-//   return (
-//     <section className="queue">
-//       <h2>Current Queue</h2>
-//       <div className='song-container'>
-//       <div className="song-list">
-//         <div className="song-item">
-//           <img src="/assets/song1.png" alt="Album cover" />
-//           <div className="song-info">
-//             <p>Count me out</p>
-//             <p className="artist">Kendrick Lamar</p>
-//           </div>
-//           {/* <img src="/assets/External link.svg" alt="External link" className="external-link" /> */}
-//           <div className="song-upvotes">(16 upvotes)</div>
-//           <img src="/assets/Remove icon.svg" alt="Remove button" className="remove-btn" />
-//         </div>
-//         <div className="song-item">
-//           <img src="/assets/song1.png" alt="Album cover" />
-//           <div className="song-info">
-//             <p>Count me out</p>
-//             <p className="artist">Kendrick Lamar</p>
-//           </div>
-//           <div className="song-upvotes">(16 upvotes)</div>
-//           <img src="/assets/Remove icon.svg" alt="Remove button" className="remove-btn" />
-//         </div>
-//         <div className="song-item">
-//           <img src="/assets/song1.png" alt="Album cover" />
-//           <div className="song-info">
-//             <p>Count me out</p>
-//             <p className="artist">Kendrick Lamar</p>
-//           </div>
-//           <div className="song-upvotes">(16 upvotes)</div>
-//           <img src="/assets/Remove icon.svg" alt="Remove button" className="remove-btn" />
-//         </div>
-//         <div className="song-item">
-//           <img src="/assets/song1.png" alt="Album cover" />
-//           <div className="song-info">
-//             <p>Count me out</p>
-//             <p className="artist">Kendrick Lamar</p>
-//           </div>
-//           <div className="song-upvotes">(16 upvotes)</div>
-//           <img src="/assets/Remove icon.svg" alt="Remove button" className="remove-btn" />
-//         </div>
-//         <div className="song-item">
-//           <img src="/assets/song1.png" alt="Album cover" />
-//           <div className="song-info">
-//             <p>Count me out</p>
-//             <p className="artist">Kendrick Lamar</p>
-//           </div>
-//           <div className="song-upvotes">(16 upvotes)</div>
-//           <img src="/assets/Remove icon.svg" alt="Remove button" className="remove-btn" />
-//         </div>
-//         <div className="song-item">
-//           <img src="/assets/song1.png" alt="Album cover" />
-//           <div className="song-info">
-//             <p>Count me out</p>
-//             <p className="artist">Kendrick Lamar</p>
-//           </div>
-//           <div className="song-upvotes">(16 upvotes)</div>
-//           <img src="/assets/Remove icon.svg" alt="Remove button" className="remove-btn" />
-//         </div>
-//         <div className="song-item">
-//           <img src="/assets/song1.png" alt="Album cover" />
-//           <div className="song-info">
-//             <p>Count me out</p>
-//             <p className="artist">Kendrick Lamar</p>
-//           </div>
-//           <div className="song-upvotes">(16 upvotes)</div>
-//           <img src="/assets/Remove icon.svg" alt="Remove button" className="remove-btn" />
-//         </div>
-//         <div className="song-item">
-//           <img src="/assets/song1.png" alt="Album cover" />
-//           <div className="song-info">
-//             <p>Count me out</p>
-//             <p className="artist">Kendrick Lamar</p>
-//           </div>
-//           <div className="song-upvotes">(16 upvotes)</div>
-//           <img src="/assets/Remove icon.svg" alt="Remove button" className="remove-btn" />
-//         </div>
-//         <div className="song-item">
-//           <img src="/assets/song1.png" alt="Album cover" />
-//           <div className="song-info">
-//             <p>Count me out</p>
-//             <p className="artist">Kendrick Lamar</p>
-//           </div>
-//           <div className="song-upvotes">(16 upvotes)</div>
-//           <img src="/assets/Remove icon.svg" alt="Remove button" className="remove-btn" />
-//         </div>
-//         <div className="song-item">
-//           <img src="/assets/song1.png" alt="Album cover" />
-//           <div className="song-info">
-//             <p>Count me out</p>
-//             <p className="artist">Kendrick Lamar</p>
-//           </div>
-//           <div className="song-upvotes">(16 upvotes)</div>
-//           <img src="/assets/Remove icon.svg" alt="Remove button" className="remove-btn" />
-//         </div>
-//       </div>
-//       </div>
-//       <button className="clear-queue">Clear Queue</button>
-//     </section>
-//   );
-// }
 
 export default Queue;
